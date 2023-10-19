adfcb799ce26387d661727cd5d66fedb5c517fe7
ETCasual <casualorient@gmail.com>
2023-10-20 01:29:51 +0800

    first commit with pwa support

---
+++ next.config.mjs
@@ -0,0 +1,31 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+import { env } from "./src/env.mjs";
+
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.mjs");
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: env.NODE_ENV === "development",
+  register: true,
+});
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+
+export default withPWA(config);
