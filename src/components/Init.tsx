52fa7fc4d9ec25d80e31ef6f5f26361292bac9a9
ETCasual <casualorient@gmail.com>
2023-10-26 18:14:32 +0800

    mounted hack

--- src/components/Init.tsx
+++ src/components/Init.tsx
@@ -1,10 +1,16 @@
 import { initializeFirestore } from "firebase/firestore";
-import type { FunctionComponent, ReactNode } from "react";
+import {
+  useEffect,
+  type FunctionComponent,
+  type ReactNode,
+  useState,
+} from "react";
 import { FirestoreProvider, useInitFirestore } from "reactfire";
 
 export const Firestore: FunctionComponent<{ children: ReactNode }> = ({
   children,
 }) => {
+  const [mounted, setMounted] = useState(false);
   const { status, data: firestoreInstance } = useInitFirestore(
     // eslint-disable-next-line @typescript-eslint/require-await
     async (firebaseApp) => {
@@ -13,6 +19,12 @@ export const Firestore: FunctionComponent<{ children: ReactNode }> = ({
       return db;
     },
   );
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return <div></div>;
 
   if (status === "loading") {
     return <div>Loading</div>;
