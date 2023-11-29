aa336ddc307b1bce207b5a39ccd0e569a5154de6
ETCasual <casualorient@gmail.com>
2023-11-29 11:55:22 +0800

    open groups btn

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -172,7 +172,7 @@ const EventPage = () => {
                   <button
                     onClick={() => router.push("/connect-camp/groups")}
                     // style={{ backgroundColor: colors.primary }}
-                    disabled
+
                     className="flex flex-row items-center justify-center border-2 border-b-[6px] border-black bg-[#ff6511] py-3 font-made font-bold text-black active:mb-[6px] active:border active:bg-opacity-80 disabled:bg-opacity-50 disabled:text-opacity-50"
                   >
                     Groups
